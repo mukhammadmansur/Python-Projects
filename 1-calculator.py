4e578aefca28c243822e69370472a0dd1b827c30
Muhammadmaqsud <elektronickaminecraft@gmail.com>
2026-05-01 22:57:16 +0500

    Refactor calculator to improve output formatting

--- 1-calculator.py
+++ 1-calculator.py
@@ -3,18 +3,22 @@ mathAc = input("Amalni kirgazing!: ")
 secondNum = int(input("Ikkinchi sonni kirgazing!: "))
 
 if ( mathAc == "+" ):
-    print(firstNum + secondNum);
+    
+    print(f"{firstNum} + {secondNum} = {firstNum + secondNum}");
 
 elif ( mathAc == "-" ):
-    print(firstNum - secondNum);
+    print(f"{firstNum} - {secondNum} = {firstNum - secondNum}");
 
 elif ( mathAc == "*" ):
-    print(firstNum * secondNum);
+    print(f"{firstNum} * {secondNum} = {firstNum * secondNum}");
 
 elif ( mathAc == "/" ):
-    print(firstNum / secondNum);
+    if secondNum != 0:
+        print(f"{firstNum} / {secondNum} = {int(firstNum / secondNum)}");
+    else :
+        print("0 ga bo'lib bo'lmaydi");
 
 elif (mathAc == "**", "darajaga ko'tar"):
-    print(firstNum ** secondNum);
+    print(f"{firstNum} ** {secondNum} = {firstNum ** secondNum}");
 
 else : print('Amallarda xatolik bor')
